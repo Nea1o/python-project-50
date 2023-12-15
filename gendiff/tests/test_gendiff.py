fb57235cf2b004fcfea5cb02d95b6a9565a3b262
Andrey Nikel <nikelandrey94@gmail.com>
2023-12-15 20:03:00 +0600

    step 4.5

--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -4,14 +4,16 @@ import pytest
 
 @pytest.fixture()
 def result_gendiff():
-    return generate_diff(
+    result = generate_diff(
         "/home/neal/Рабочий стол/Study/Project2"
         "/gendiff/tests/fixtures/file1.json",
         "/home/neal/Рабочий стол/Study/Project2"
         "/gendiff/tests/fixtures/file2.json")
+    return result
 
 
 def test_gendiff_func(result_gendiff):
-    assert result_gendiff == ("{ \n  - follow: false \n    host: hexlet.io"
+    result_func = result_gendiff
+    assert result_func == ("{ \n  - follow: false \n    host: hexlet.io"
                               " \n  - proxy: 123.234.53.22 \n  - timeout: 50"
                               " \n  + timeout: 20 \n  + verbose: true \n}")
