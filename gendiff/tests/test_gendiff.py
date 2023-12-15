a0df330f1a52e12420813650792fdde32cde85a7
Andrey Nikel <nikelandrey94@gmail.com>
2023-12-15 20:04:35 +0600

    step 4.6

--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -15,5 +15,5 @@ def result_gendiff():
 def test_gendiff_func(result_gendiff):
     result_func = result_gendiff
     assert result_func == ("{ \n  - follow: false \n    host: hexlet.io"
-                              " \n  - proxy: 123.234.53.22 \n  - timeout: 50"
-                              " \n  + timeout: 20 \n  + verbose: true \n}")
+                           " \n  - proxy: 123.234.53.22 \n  - timeout: 50"
+                           " \n  + timeout: 20 \n  + verbose: true \n}")
