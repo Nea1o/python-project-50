8c020dd43585fad155e6421fc84f06ed7fdde2f6
Andrey Nikel <nikelandrey94@gmail.com>
2024-02-02 16:29:01 +0600

    test

--- gendiff/generate.py
+++ gendiff/generate.py
@@ -1,4 +1,5 @@
-from pars_file import pars_file
+import yaml
+import json
 
 
 def generate_diff(first_file, second_file):
@@ -6,17 +7,33 @@ def generate_diff(first_file, second_file):
     file2 = pars_file(second_file)
     result_dict = {}
     for key in sorted(file1 | file2):
-        if key in file1 and key not in file2:
-            result_dict[f"  - {key}"] = file1[key]
-        if key in file2 and key not in file1:
-            result_dict[f"  + {key}"] = file2[key]
-        if key in file1 and key in file2 and file1[key] == file2[key]:
+        if key not in file1 or key not in file2:
+            if key in file1:
+                result_dict[f"  - {key}"] = file1[key]
+                print(result_dict[f"  - {key}"])
+            if key in file2:
+                result_dict[f"  + {key}"] = file2[key]
+                print(result_dict[f"  + {key}"])
+        if key in file1 and key in file2:
             result_dict[f"    {key}"] = file1[key]
-        if key in file1 and key in file2 and file1[key] != file2[key]:
-            result_dict[f"  - {key}"] = file1[key]
-            result_dict[f"  + {key}"] = file2[key]
-    result = dict_in_str(result_dict)
+            if isinstance(file1[key], dict) and isinstance(file2[key], dict):
+                generate_diff(file1[key], file2[key])
+            else:
+                result_dict[f"  - {key}"] = file1[key]
+                result_dict[f"  + {key}"] = file2[key]
+
+    result = stylish(result_dict)
     return result
+
+
+"""        if key in file1 and key in file2 and file1[key] == file2[key]:
+            print("2")
+            result_dict[f"    {key}"] = file1[key]
+            generate_diff(file1[key], file2[key])
+        if key in file1 and key in file2 and file1[key] != file2[key]:
+            print("3")
+            result_dict[f"  - {key}"] = file1[key]
+            result_dict[f"  + {key}"] = file2[key]"""
 
 
 def dict_in_str(dct):
@@ -28,3 +45,29 @@ def dict_in_str(dct):
             result += f"{keys}: {values} \n"
     result += "}"
     return result
+
+
+def pars_file(path_file):
+    if isinstance(path_file, str):
+        if str(path_file).endswith(".json"):
+            return json.load(open(path_file))
+        if str(path_file).endswith(".yaml"):
+            return yaml.load(open(path_file), Loader=yaml.SafeLoader)
+    else:
+        return path_file
+
+
+def stylish(value, replacer='  ', space_count=1, _lvl=1):
+    if isinstance(value, dict):
+        result = '{\n'
+        for el, val in value.items():
+            result += f'{replacer * space_count * _lvl}{el}: '
+            result += stylish(val, replacer, space_count, _lvl + 1) + '\n'
+        result += replacer * space_count * (_lvl - 1) + '}'
+    else:
+        result = str(value)
+    return result
+
+
+print(stylish(generate_diff("/home/tosh/code/python-project-50/gendiff/tests/fixtures/file3.yaml",
+                            "/home/tosh/code/python-project-50/gendiff/tests/fixtures/file4.yaml")))
--- gendiff/pars_file.py
+++ gendiff/pars_file.py
@@ -2,8 +2,10 @@ import json
 import yaml
 
 
-def pars_file(path_file):
-    if str(path_file).endswith(".json"):
-        return json.load(open(path_file))
-    if str(path_file).endswith(".yaml"):
-        return yaml.load(open(path_file), Loader=yaml.SafeLoader)
+
+
+
+file_1 = yaml.load(open("/home/tosh/code/python-project-50/gendiff/tests/fixtures/file3.yaml"), Loader=yaml.SafeLoader)
+file_2 = yaml.load(open("/home/tosh/code/python-project-50/gendiff/tests/fixtures/file4.yaml"), Loader=yaml.SafeLoader)
+print(file_1)
+print(file_2)
