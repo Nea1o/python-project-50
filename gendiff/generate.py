8fb23e8a6c263a45f08ac125798b523beb0e81f5
Andrey Nikel <nikelandrey94@gmail.com>
2024-02-05 16:23:01 +0600

    gendiff func

--- gendiff/generate.py
+++ gendiff/generate.py
@@ -3,48 +3,22 @@ import json
 
 
 def generate_diff(first_file, second_file):
+    result_dict = {}
     file1 = pars_file(first_file)
     file2 = pars_file(second_file)
-    result_dict = {}
     for key in sorted(file1 | file2):
-        if key not in file1 or key not in file2:
-            if key in file1:
-                result_dict[f"  - {key}"] = file1[key]
-                print(result_dict[f"  - {key}"])
-            if key in file2:
-                result_dict[f"  + {key}"] = file2[key]
-                print(result_dict[f"  + {key}"])
-        if key in file1 and key in file2:
-            result_dict[f"    {key}"] = file1[key]
-            if isinstance(file1[key], dict) and isinstance(file2[key], dict):
-                generate_diff(file1[key], file2[key])
-            else:
-                result_dict[f"  - {key}"] = file1[key]
-                result_dict[f"  + {key}"] = file2[key]
-
-    result = stylish(result_dict)
-    return result
-
-
-"""        if key in file1 and key in file2 and file1[key] == file2[key]:
-            print("2")
-            result_dict[f"    {key}"] = file1[key]
-            generate_diff(file1[key], file2[key])
-        if key in file1 and key in file2 and file1[key] != file2[key]:
-            print("3")
-            result_dict[f"  - {key}"] = file1[key]
-            result_dict[f"  + {key}"] = file2[key]"""
-
-
-def dict_in_str(dct):
-    result = "{ \n"
-    for keys, values in dct.items():
-        if type(values) is bool:
-            result += f"{keys}: {str(values).lower()} \n"
-        else:
-            result += f"{keys}: {values} \n"
-    result += "}"
-    return result
+        if key not in file1:
+            result_dict[f"+ {key}"] = file2[key]
+        elif key not in file2:
+            result_dict[f"- {key}"] = file1[key]
+        elif file1[key] == file2[key]:
+            result_dict[f"{key}"] = file1[key]
+        elif isinstance(file1[key], dict) and isinstance(file2[key], dict):
+            result_dict[f"{key}"] = generate_diff(file1[key], file2[key])
+        elif key in file1 and key in file2:
+            result_dict[f"- {key}"] = file1[key]
+            result_dict[f"+ {key}"] = file2[key]
+    return result_dict
 
 
 def pars_file(path_file):
@@ -57,13 +31,20 @@ def pars_file(path_file):
         return path_file
 
 
-def stylish(value, replacer='  ', space_count=1, _lvl=1):
+def stylish(value, replacer='    ', space_count=1, level=1):
     if isinstance(value, dict):
         result = '{\n'
         for el, val in value.items():
-            result += f'{replacer * space_count * _lvl}{el}: '
-            result += stylish(val, replacer, space_count, _lvl + 1) + '\n'
-        result += replacer * space_count * (_lvl - 1) + '}'
+            if el[0] == "+":
+                result += f'{replacer * space_count * (level-1)}  + {el[2::]}: '
+                result += stylish(val, replacer, space_count, level + 1) + '\n'
+            elif el[0] == "-":
+                result += f'{replacer * space_count * (level-1)}  - {el[2::]}: '
+                result += stylish(val, replacer, space_count, level + 1) + '\n'
+            else:
+                result += f'{replacer * space_count * level}{el}: '
+                result += stylish(val, replacer, space_count, level + 1) + '\n'
+        result += replacer * space_count * (level - 1) + '}'
     else:
         result = str(value)
     return result
